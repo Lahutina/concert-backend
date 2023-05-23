5ce1619c1dd3b99fd46645b28f9ea1a952bad4ba
Oksana Lahutina <oksankalahutina@gmail.com>
2023-05-23 16:27:03 +0300

    Csrf method update

--- src/main/java/com/concert/config/SecurityConfig.java
+++ src/main/java/com/concert/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -23,7 +24,7 @@ public class SecurityConfig {
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
-        http.csrf().disable()
+        http.csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests((authorize) ->
                         authorize.anyRequest().authenticated()).httpBasic(Customizer.withDefaults());
         return http.build();
